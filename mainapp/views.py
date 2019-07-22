bc278ce7a8e536d13142bac4213e5d6e1317087b
Iakubov <max.iakubov@gmail.com>
2019-07-22 23:32:59 +0300

    Добавил контроллеры и URL

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def product(request):
+    return render(request, 'mainapp/product.html')
+
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
