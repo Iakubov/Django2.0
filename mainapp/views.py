7644a3be0cb4319dae8c6039173b4ba66626fa85
Iakubov <max.iakubov@gmail.com>
2019-07-29 01:14:38 +0300

    Dynamic contacts template

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,8 +18,31 @@ def product(request):
 
 
 def contacts(request):
+
+    content = [
+        {
+            "city": "Los Angeles",
+            "phone": "8-800-800-800",
+            "email": "los-a@gmail.com",
+            "adress": "LA",
+        },
+        {
+            "city": "Houston",
+            "phone": "8-555-555-555",
+            "email": "houston@gmail.com",
+            "adress": "HU",
+        },
+        {
+            "city": "Austin",
+            "phone": "8-9990-80990-8090",
+            "email": "austin@gmail.com",
+            "adress": "AU",
+        },
+    ]
+
     context = {
-        'page_title': 'Contacts'
+        'page_title': 'Contacts',
+        'content': content,
     }
 
-    return render(request, 'mainapp/contacts.html', context)
+    return render(request, 'mainapp/contacts.html', context, content)
