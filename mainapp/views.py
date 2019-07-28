dd0e9eb10ef87b3f0343ecb9aa859ea8bde6747c
Iakubov <max.iakubov@gmail.com>
2019-07-29 01:24:40 +0300

    json content loader

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
@@ -18,31 +19,11 @@ def product(request):
 
 
 def contacts(request):
-
-    content = [
-        {
-            "city": "Los Angeles",
-            "phone": "8-800-800-800",
-            "email": "los-a@gmail.com",
-            "adress": "LA",
-        },
-        {
-            "city": "Houston",
-            "phone": "8-555-555-555",
-            "email": "houston@gmail.com",
-            "adress": "HU",
-        },
-        {
-            "city": "Austin",
-            "phone": "8-9990-80990-8090",
-            "email": "austin@gmail.com",
-            "adress": "AU",
-        },
-    ]
+    with open("geekshop/content.json", "r") as file:
+        content = json.load(file)
 
     context = {
         'page_title': 'Contacts',
         'content': content,
     }
-
-    return render(request, 'mainapp/contacts.html', context, content)
+    return render(request, 'mainapp/contacts.html', context)
