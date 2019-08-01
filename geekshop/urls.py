e2420d0ed9dd27be4e967d58164d8d52e791905d
Iakubov <max.iakubov@gmail.com>
2019-08-01 04:45:45 +0300

    Настроил проект для работы с медиафайлами. Подправил style.css

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
 
+
 urlpatterns = [
     path('admin/', admin.site.urls,),
     path('', mainapp.main, name='index'),
     path('product/', mainapp.product,name='products'),
     path('contacts/', mainapp.contacts,name='contacts'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
