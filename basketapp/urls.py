d28dd80d6568f5d32b999810f047f70486b8205f
Iakubov <max.iakubov@gmail.com>
2019-08-09 23:43:32 +0300

    basket add

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -4,5 +4,7 @@ import basketapp.views as basketapp
 app_name = 'basketapp'
 
 urlpatterns = [
-    path('auth/login/', basketapp.index, name='index'),
+    path('', basketapp.index, name='index'),
+    path('add/<int:pk>/', basketapp.basket_add, name='add'),
+    path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
 ]
