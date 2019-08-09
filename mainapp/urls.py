9a42198604deb01e4fe7007866e50d8f5bf3dcc7
Iakubov <max.iakubov@gmail.com>
2019-08-09 21:05:02 +0300

    category

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.main, name='index'),
     path('product/', mainapp.product, name='products'),
+    path('category/<int:pk>/', mainapp.category, name='category'),
     path('contacts/', mainapp.contacts, name='contacts'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import json
-from mainapp.models import Product
+from mainapp.models import Product, ProductCategory
 
-# Create your views here.
+
+def get_products_menu():
+    return ProductCategory.objects.all()
 
 
 def main(request):
@@ -16,9 +18,31 @@ def main(request):
 
 def product(request):
     context = {
-        'page_title': 'Products'
+        'page_title': 'Products',
+        'products_menu': get_products_menu(),
     }
     return render(request, 'mainapp/product.html', context)
+
+
+def category(request, pk):
+    pk = int(pk)
+    if pk == 0:
+        category = {
+            'pk': 0,
+            'name': 'all'
+        }
+        category_products = Product.objects.all()
+    else:
+        category = get_object_or_404(ProductCategory, pk=pk)
+        category_products = category.product_set.all()
+
+    context = {
+        'page_title': 'Products',
+        'products_menu': get_products_menu(),
+        'category_products': category_products,
+        'category': category
+    }
+    return render(request, 'mainapp/category_products.html', context)
 
 
 def contacts(request):
