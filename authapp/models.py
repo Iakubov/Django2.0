76c96d10848e435836ace6c232dd1b025dd7b935
Iakubov <max.iakubov@gmail.com>
2019-10-06 17:58:14 +0300

    VK authentication

--- authapp/models.py
+++ authapp/models.py
@@ -2,12 +2,14 @@ from datetime import timedelta
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='Age')
+    age = models.PositiveIntegerField(verbose_name='Age', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(
@@ -19,3 +21,29 @@ class ShopUser(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'M'),
+        (FEMALE, 'W')
+    )
+
+    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE,
+                             null=False, db_index=True, unique=True)
+    tagLine = models.CharField(verbose_name='tags', max_length=128, blank=True)
+    aboutMe = models.CharField(verbose_name='about', max_length=512, blank=True)
+    gender = models.CharField(verbose_name='sex', max_length=1,
+                              choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(self, instance, created, sender, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(self, instance, sender, **kwargs):
+        instance.shopuserprofile.save()
