f58270cc7bff48c1acabb848059f2317ce3940c0
iakubov <max.iakubov@gmail.com>
2019-11-09 09:06:39 +0300

    debug toolbar

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -17,3 +17,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+   import debug_toolbar
+
+   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
